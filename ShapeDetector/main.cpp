7eb35e7fbfa514860baa2a034a728a120dcf5b6e
Etienne Pasteur <etienne.pasteur@epitech.eu>
2017-05-13 00:14:51 +0200

    Add image and shape detection

--- ShapeDetector/main.cpp
+++ ShapeDetector/main.cpp
@@ -1,95 +1,125 @@
-#include "opencv2/imgproc.hpp"
-#include "opencv2/highgui.hpp"
-#include <math.h>
 #include <iostream>
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
 
 using namespace cv;
 using namespace std;
 
-static void help()
-{
-    cout
-            << "\nThis program illustrates the use of findContours and drawContours\n"
-            << "The original image is put up along with the image of drawn contours\n"
-            << "Usage:\n"
-            << "./contours2\n"
-            << "\nA trackbar is put up which controls the contour level from -3 to 3\n"
-            << endl;
+
+int thresh = 50, N = 1;
+
+// helper function:
+// finds a cosine of angle between vectors
+// from pt0->pt1 and from pt0->pt2
+static double angle(Point pt1, Point pt2, Point pt0) {
+    double dx1 = pt1.x - pt0.x;
+    double dy1 = pt1.y - pt0.y;
+    double dx2 = pt2.x - pt0.x;
+    double dy2 = pt2.y - pt0.y;
+    return (dx1 * dx2 + dy1 * dy2) / sqrt((dx1 * dx1 + dy1 * dy1) * (dx2 * dx2 + dy2 * dy2) + 1e-10);
 }
 
-const int w = 500;
-int levels = 3;
+// returns sequence of squares detected on the image.
+// the sequence is stored in the specified memory storage
+static void findSquares(const Mat &image, vector<vector<Point> > &squares) {
+    squares.clear();
 
-vector<vector<Point> > contours;
-vector<Vec4i> hierarchy;
+    Mat pyr, timg, gray0(image.size(), CV_8U), gray;
 
-static void on_trackbar(int, void*)
-{
-    Mat cnt_img = Mat::zeros(w, w, CV_8UC3);
-    int _levels = levels - 3;
-    drawContours( cnt_img, contours, _levels <= 0 ? 3 : -1, Scalar(128,255,255),
-                  3, LINE_AA, hierarchy, std::abs(_levels) );
+    // down-scale and upscale the image to filter out the noise
+    pyrDown(image, pyr, Size(image.cols / 2, image.rows / 2));
+    pyrUp(pyr, timg, image.size());
+    vector<vector<Point> > contours;
 
-    imshow("contours", cnt_img);
-}
+    // find squares in every color plane of the image
+    for (int c = 0; c < 3; c++) {
+        int ch[] = {c, 0};
+        mixChannels(&timg, 1, &gray0, 1, ch, 1);
 
-int main( int argc, char** argv)
-{
-    cv::CommandLineParser parser(argc, argv, "{help h||}");
-    if (parser.has("help"))
-    {
-        help();
-        return 0;
-    }
-    Mat img = Mat::zeros(w, w, CV_8UC1);
-    //Draw 6 faces
-    for( int i = 0; i < 6; i++ )
-    {
-        int dx = (i%2)*250 - 30;
-        int dy = (i/2)*150;
-        const Scalar white = Scalar(255);
-        const Scalar black = Scalar(0);
+        // try several threshold levels
+        for (int l = 0; l < N; l++) {
+            // hack: use Canny instead of zero threshold level.
+            // Canny helps to catch squares with gradient shading
+            if (l == 0) {
+                // apply Canny. Take the upper threshold from slider
+                // and set the lower to 0 (which forces edges merging)
+                Canny(gray0, gray, 0, thresh, 5);
+                // dilate canny output to remove potential
+                // holes between edge segments
+                dilate(gray, gray, Mat(), Point(-1, -1));
+            } else {
+                // apply threshold if l!=0:
+                //     tgray(x,y) = gray(x,y) < (l+1)*255/N ? 255 : 0
+                gray = gray0 >= (l + 1) * 255 / N;
+            }
 
-        if( i == 0 )
-        {
-            for( int j = 0; j <= 10; j++ )
-            {
-                double angle = (j+5)*CV_PI/21;
-                line(img, Point(cvRound(dx+100+j*10-80*cos(angle)),
-                                cvRound(dy+100-90*sin(angle))),
-                     Point(cvRound(dx+100+j*10-30*cos(angle)),
-                           cvRound(dy+100-30*sin(angle))), white, 1, 8, 0);
+            // find contours and store them all as a list
+            findContours(gray, contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
+
+            vector<Point> approx;
+
+            // test each contour
+            for (size_t i = 0; i < contours.size(); i++) {
+                // approximate contour with accuracy proportional
+                // to the contour perimeter
+                approxPolyDP(Mat(contours[i]), approx, arcLength(Mat(contours[i]), true) * 0.02, true);
+
+                // square contours should have 4 vertices after approximation
+                // relatively large area (to filter out noisy contours)
+                // and be convex.
+                // Note: absolute value of an area is used because
+                // area may be positive or negative - in accordance with the
+                // contour orientation
+                if (approx.size() == 4 &&
+                    fabs(contourArea(Mat(approx))) > 10000 &&
+                    isContourConvex(Mat(approx))) {
+                    double maxCosine = 0;
+
+                    for (int j = 2; j < 5; j++) {
+                        // find the maximum cosine of the angle between joint edges
+                        double cosine = fabs(angle(approx[j % 4], approx[j - 2], approx[j - 1]));
+                        maxCosine = MAX(maxCosine, cosine);
+                    }
+
+                    // if cosines of all angles are small
+                    // (all angles are ~90 degree) then write quandrange
+                    // vertices to resultant sequence
+                    if (maxCosine < 0.2)
+                        squares.push_back(approx);
+                }
             }
         }
-
-        ellipse( img, Point(dx+150, dy+100), Size(100,70), 0, 0, 360, white, -1, 8, 0 );
-        ellipse( img, Point(dx+115, dy+70), Size(30,20), 0, 0, 360, black, -1, 8, 0 );
-        ellipse( img, Point(dx+185, dy+70), Size(30,20), 0, 0, 360, black, -1, 8, 0 );
-        ellipse( img, Point(dx+115, dy+70), Size(15,15), 0, 0, 360, white, -1, 8, 0 );
-        ellipse( img, Point(dx+185, dy+70), Size(15,15), 0, 0, 360, white, -1, 8, 0 );
-        ellipse( img, Point(dx+115, dy+70), Size(5,5), 0, 0, 360, black, -1, 8, 0 );
-        ellipse( img, Point(dx+185, dy+70), Size(5,5), 0, 0, 360, black, -1, 8, 0 );
-        ellipse( img, Point(dx+150, dy+100), Size(10,5), 0, 0, 360, black, -1, 8, 0 );
-        ellipse( img, Point(dx+150, dy+150), Size(40,10), 0, 0, 360, black, -1, 8, 0 );
-        ellipse( img, Point(dx+27, dy+100), Size(20,35), 0, 0, 360, white, -1, 8, 0 );
-        ellipse( img, Point(dx+273, dy+100), Size(20,35), 0, 0, 360, white, -1, 8, 0 );
     }
-    //show the faces
-    namedWindow( "image", 1 );
-    imshow( "image", img );
-    //Extract the contours so that
-    vector<vector<Point> > contours0;
-    findContours( img, contours0, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
+}
 
-    contours.resize(contours0.size());
-    for( size_t k = 0; k < contours0.size(); k++ )
-        approxPolyDP(Mat(contours0[k]), contours[k], 3, true);
 
-    namedWindow( "contours", 1 );
-    createTrackbar( "levels+3", "contours", &levels, 7, on_trackbar );
+// the function draws all the squares in the image
+static void drawSquares(Mat &image, const vector<vector<Point> > &squares) {
+    for (size_t i = 0; i < squares.size(); i++) {
+        std::cout << "Draw square n" << i << std::endl;
+        const Point *p = &squares[i][0];
+        int n = (int) squares[i].size();
+        polylines(image, &p, &n, 1, true, Scalar(0, 255, 0), 3, LINE_AA);
+    }
+    imshow("Lettrine", image);
+}
 
-    on_trackbar(0,0);
-    waitKey();
+
+int main(int argc, char **argv) {
+    namedWindow("Lettrine", 1);
+    vector<vector<Point> > squares;
+
+    Mat image = imread("3.jpg", 1);
+    findSquares(image, squares);
+    drawSquares(image, squares);
+
+    for (;;) {
+        char c = (char) waitKey();
+        if (c == 27)
+            break;
+    }
 
     return 0;
 }
