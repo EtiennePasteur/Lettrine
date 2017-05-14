d73f04d96e4a141ccd26bb952af2ab14dc82f59f
Etienne Pasteur <etienne.pasteur@epitech.eu>
2017-05-14 08:55:01 +0200

    Refacto + finishing detection of images

--- sources/extractor.cpp
+++ sources/extractor.cpp
@@ -8,6 +8,11 @@
 using namespace cv;
 using namespace std;
 
+typedef struct s_pos_contour {
+    Point max;
+    Point min;
+    vector<Point> contour;
+} t_pos_contour;
 
 void bwareaopen(Mat &img, int size) {
     CBlobResult blobs;
@@ -22,54 +27,56 @@ void bwareaopen(Mat &img, int size) {
     img = newimg;
 }
 
-static void newFind(const Mat &image, vector<vector<Point> > &squares) {
+void findPics(const Mat &image, std::vector<t_pos_contour> &contoursPos) {
     vector<vector<Point> > contours;
-    vector<Vec4i> hierarchy;
-
-    //Extract the contours so that
     vector<vector<Point> > contours0;
-    findContours(image, contours0, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-
+    vector<Vec4i> h;
+    findContours(image, contours0, h, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
     contours.resize(contours0.size());
     for (size_t k = 0; k < contours0.size(); k++)
         approxPolyDP(Mat(contours0[k]), contours[k], 3, true);
 
-    Mat drawing = Mat::zeros(image.size().height, image.size().width, CV_8UC3);
-
-
-
-
-
-    drawContours(drawing, contours, 3,  Scalar(0,255,0), 1, 8, vector<Vec4i>(), 0, Point());
-
-    for (int i = 0; i < contours.size(); i++)
-    {
-        std::cout << i << std::endl;
+    bool remove = false;
+    t_pos_contour valueContour;
+    double seuilX = image.size().width * 0.07;
+    double seuilY = image.size().height * 0.07;
+    for (std::vector<vector<Point>>::iterator itV = contours.begin(); itV != contours.end();) {
+        valueContour.max = {0, 0};
+        valueContour.min = {image.size().width, image.size().height};
+        valueContour.contour = *itV;
+        for (std::vector<Point>::iterator itP = itV->begin(); itP != itV->end(); itP++) {
+            if (itP->x < 10 || itP->x > image.size().width - 10 || itP->y < 10 || itP->y > image.size().height - 10) {
+                itV = contours.erase(itV);
+                remove = true;
+                break;
+            }
+            if (valueContour.max.x < itP->x)
+                valueContour.max.x = itP->x;
+            if (valueContour.max.y < itP->y)
+                valueContour.max.y = itP->y;
+            if (valueContour.min.x > itP->x)
+                valueContour.min.x = itP->x;
+            if (valueContour.min.y > itP->y)
+                valueContour.min.y = itP->y;
+        }
+        if (!remove) {
+            ++itV;
+        } else {
+            remove = false;
+            continue;
+        }
+        if (valueContour.max.x - valueContour.min.x > seuilX && valueContour.max.y - valueContour.min.y > seuilY)
+            contoursPos.push_back(valueContour);
     }
-    imshow("Lettrine", drawing);
-
 }
 
+
+
 void extractPics(const std::string &path) {
-    namedWindow("Lettrine", 1);
-    vector<vector<Point>> squares;
-
     Mat imageP = imread(path, 1);
-
+    std::vector<t_pos_contour> contoursPos;
     cvtColor(imageP, imageP, CV_RGB2GRAY);
     threshold(imageP, imageP, 75.0, 255.0, THRESH_BINARY_INV);
-
     bwareaopen(imageP, 800);
-
-    resize(imageP, imageP, Size(0, 0), 0.4, 0.4);
-
-    newFind(imageP, squares);
-
-    for (;;) {
-        int c = waitKey();
-        if (c == 13 || c == 10)
-            break;
-        if (c == 27)
-            exit(0);
-    }
+    findPics(imageP, contoursPos);
 }
