7abb062bf87f33504850db22c1896ad6640a64e4
aguado_e <enzo.aguado@epitech.eu>
2017-05-18 11:55:35 +0200

    add Headers for functions prototypes

--- Sources/Headers/ThreadConductor.hpp
+++ Sources/Headers/ThreadConductor.hpp
@@ -5,4 +5,12 @@
 #ifndef LETTRINE_THREADCONDUCTOR_HPP
 #define LETTRINE_THREADCONDUCTOR_HPP
 
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <Extractor.hpp>
+
+void processImage(std::queue<std::tuple<std::string, std::string>> &files);
+void startProcessThreads(std::queue<std::tuple<std::string, std::string>> &files);
+
 #endif //LETTRINE_THREADCONDUCTOR_HPP
